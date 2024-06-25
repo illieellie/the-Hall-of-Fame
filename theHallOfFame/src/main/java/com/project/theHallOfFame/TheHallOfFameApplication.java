9deb6af18be3c5670a2c5d4f26dacad6388d0f2e
imellie <ssul4666@naver.com>
2024-06-25 17:25:50 +0900

    create project file

---
+++ theHallOfFame/src/main/java/com/project/theHallOfFame/TheHallOfFameApplication.java
@@ -0,0 +1,13 @@
+package com.project.theHallOfFame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheHallOfFameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheHallOfFameApplication.class, args);
+	}
+
+}
---
+++ theHallOfFame/src/test/java/com/project/theHallOfFame/TheHallOfFameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.theHallOfFame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheHallOfFameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
